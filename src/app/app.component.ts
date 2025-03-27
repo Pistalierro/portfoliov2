874b7115953fbc1d02f3af805f5b660520e82036
Anatoliy Sysoienko <pistalierro@gmail.com>
2025-03-27 18:54:44 +0200

    Добавил картинки и компонент hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
 import {Component} from '@angular/core';
+import {NavbarComponent} from '../features/navbar/navbar.component';
+import {AboutComponent} from '../features/about/about.component';
+import {HeroComponent} from '../features/hero/hero.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    NavbarComponent,
+    AboutComponent,
+    HeroComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{html,ts}"
   ],
   theme: {
-    extend: {}
+    extend: {
+      keyframes: {
+        zoom: {
+          '0%, 100%': {transform: 'scale(1)'},
+          '50%': {transform: 'scale(1.1)'}
+        }
+      }
+    },
+    animation: {
+      zoom: 'zoom 15s ease-in-out infinite'
+    }
   },
   plugins: []
 }
