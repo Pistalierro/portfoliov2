c2b5a8e9b2e3e327bd414b3e8aa11592b233315c
Anatoliy Sysoienko <pistalierro@gmail.com>
2025-04-24 13:37:38 +0300

    Убрал компоненты инструменты и методологии. Вместо них сделал skill-approach

---
+++ src/pages/home/sections/skills/skill-approach/skill-approach.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-approach',
+  imports: [],
+  templateUrl: './skill-approach.component.html',
+  styleUrl: './skill-approach.component.scss'
+})
+export class SkillApproachComponent {
+
+}
